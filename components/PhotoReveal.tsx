b3174433a105ecccf048780b446e9086326f1433
Favour Baraka <favourbaraka1@gmail.com>
2026-09-10 14:02:52 +0300

    added fade in on about

--- components/PhotoReveal.tsx
+++ components/PhotoReveal.tsx
@@ -26,7 +26,7 @@ export default function PhotoReveal({
     <motion.div
       initial={{ opacity: 0 }}
       animate={ready ? { opacity: 1 } : {}}
-      transition={{ duration: 0.9, delay, ease: [0.23, 1, 0.32, 1] }}
+      transition={{ duration: 0.9, delay, ease: [0.45, 0, 0.15, 1] }}
       className="absolute inset-0"
     >
       {children}
